f3ad77f6a11ef510a7bb00f184d1fd0d2e801d5a
Jameschen486 <james.chen486@gmail.com>
2024-06-23 19:59:11 +1000

    implemented method for users to register a new account

--- backend/authentication.py
+++ backend/authentication.py
@@ -3,22 +3,18 @@ import random
 import dbAcc
 import dbAcc
 
-def login(username, password) :
-    data_store = getData()
-    validUser = False
-    user_login = None
+class HTTPError(Exception):
+    def __init__(self, status_code, message):
+        self.status_code = status_code
+        self.message = message
 
-    class HTTPError(Exception):
-        def __init__(self, status_code, message):
-            self.status_code = status_code
-            self.message = message
-            
+def login(username, password) :
+    user_login = None
     user = dbAcc.get_user_by_email(username)
     hashpass = getHashOf(password)
 
     if getHashOf(password) == user['password']:
         user_login = user
-        validUser = True
         token = str(random.randint(0, 500000))
         user['tokens'].append(token)
         return {
@@ -27,7 +23,17 @@ def login(username, password) :
         }
 
     else:
-        raise HTTPError(400, 'Invalid login, try again')
+        raise HTTPError(400, 'Invalid email or password, please try again')
+def userRegister(username, password, firstName, lastName, role):
+
+    user = dbAcc.get_user_by_email(username)
+    if user['username'] == username:
+        raise HTTPError(400, 'User already exists, please try login or reset password')
+    else:
+        hashedPassword = getHashOf(password)
+        dbAcc.create_user(username, hashedPassword, firstName, lastName, role)
+    return
+
 
 def getHashOf(password):
     return hashlib.md5(password.encode()).hexdigest()
