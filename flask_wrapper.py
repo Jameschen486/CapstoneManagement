d27a80948854b1f44a9056e969598480584f766b
AndyWang9797 <andywang971@gmail.com>
2024-06-23 14:28:11 +1000

    Added group functions create, view and join, the code still needs update with our database functions tho

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+from src import groups
 from users import login
+
+MAX_STUDENT_PER_GROUP = 6
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +17,28 @@ def auth_login():
     username = request.form['username']
     password = request.form['password']
     return jsonify(login(username,password))
+
+@app.route('/create_group', methods=['POST'])
+def create_group_endpoint():
+    data = request.form()
+    group_name = data.get('name')
+    group_description = data.get('description')
+    creator_id = data.get('creator_id')
+
+    response, status_code = groups.create_group(group_name, group_description, creator_id)
+    return jsonify(response), status_code
+
+@app.route('/groups', methods=['GET'])
+def view_groups_route():
+    return jsonify(groups.view_groups())
+
+@app.route('/join_group', methods=['POST'])
+def join_group_route():
+    data = request.form()
+    group_id = data.get('group_id')
+    student_id = data.get('student_id')
+    response, status_code = groups.join_group(group_id, student_id, MAX_STUDENT_PER_GROUP)
+    return jsonify(response), status_code
 
 if __name__ == "__main__":
     app.run(debug=True)
