f24e5b3560c5ec41f677987aeb413b16b56418e6
chichun2002 <chichun2002@gmail.com>
2024-06-25 04:21:22 +1000

    Tested Login and Register Both Work in Docker Fixed Docker Port not working on backend updated File Names

--- backend/authentication.py
+++ backend/authentication.py
@@ -2,6 +2,19 @@ import hashlib
 import random
 import dbAcc
 import dbAcc
+import jwt
+
+key = "rngwarriors"
+
+def jwt_encode(payload):
+    return jwt.encode(payload, key, algorithm="HS256")
+
+def jwt_decode(token):
+    try: 
+        payload = jwt.decode(token, key, algorithms="HS256")
+        return payload
+    except:
+        return None
 
 class HTTPError(Exception):
     def __init__(self, status_code, message):
@@ -9,25 +22,26 @@ class HTTPError(Exception):
         self.message = message
 
 def login(email, password) :
-    user_login = None
     user = dbAcc.get_user_by_email(email)
-    hashpass = getHashOf(password)
-
-    if getHashOf(password) == user['password']:
-        user_login = user
-        token = str(random.randint(0, 500000))
-        user['tokens'].append(token)
-        return {
-            'token': token,
-            'authUserId': user_login['uId']
+    if user is None:
+        raise HTTPError(400, 'Invalid email or password, please try again')
+    
+    if getHashOf(password) == user[4]:
+        payload = {
+            'userid': user[0],
+            'role': user[5]
         }
-
+        token = jwt_encode(payload)
+        return {
+            'userid': user[0],
+            'token': token
+        }
     else:
         raise HTTPError(400, 'Invalid email or password, please try again')
-def userRegister(email, password, firstName, lastName, role):
+    
+def register(email, password, firstName, lastName, role=0):
+    if dbAcc.get_user_by_email(email) is not None:
 
-    user = dbAcc.get_user_by_email(email)
-    if user['username'] == email:
         raise HTTPError(400, 'User already exists, please try login or reset password')
     else:
         hashedPassword = getHashOf(password)
