98e2a0df2f8b464e4d356b5bb862ca582e72c497
AndyWang9797 <andywang971@gmail.com>
2024-06-25 18:18:32 +1000

    Added view group details view join_request and handle join request functions

--- backend/groups.py
+++ backend/groups.py
@@ -1,6 +1,6 @@
 import psycopg2
 from backend.error import InputError, AccessError
-from backend.dbAcc import create_group, get_all_groups, get_groupcount_by_name, get_user_by_id, get_group_members
+from backend.dbAcc import create_group, get_all_groups, get_groupcount_by_name, get_user_by_id, get_group_members, create_join_request, add_user_to_group, remove_all_join_requests, remove_join_request, get_group_by_id, get_join_requests
 
 def create_group(group_name, creator_id):
 
@@ -32,8 +32,38 @@ def join_group(group_id, student_id, group_capacity):
         raise AccessError(description="Group is full")
     
     # Send a join request
-    # create_join_request(student_id, group_id)
-    # return {"message": "Join request sent successfully!"}, 201
+    create_join_request(student_id, group_id)
+    return {"message": "Join request sent successfully!"}, 201
 
-def handle_join_request(creator_id, applicant_id, group_id):
-    return True
+def handle_join_request(creator_id, applicant_id, group_id, accept, group_capacity):
+    if not is_user_creator(creator_id, group_id):
+        raise AccessError(description="You do not have access to accept/reject join requests")
+    
+    if accept:
+        if len(get_group_members(group_id)) >= group_capacity:
+            raise AccessError(description="Group is full")
+        
+        add_user_to_group(applicant_id, group_id)
+        remove_all_join_requests(applicant_id)
+        return  {"message": f"User {applicant_id} added to your group."}, 201
+        # TODO In next sprint send notification to applicant.
+    else:
+        remove_join_request(applicant_id, group_id)
+        return {"message": f"User {applicant_id} rejected."}, 201
+        # TODO In next sprint send notification to applicant.
+
+def view_group_details(group_id):
+    group_details = get_group_by_id(group_id)
+    
+    if not group_details:
+        raise InputError(description="Group not found")
+
+    return group_details, 200
+
+def view_join_requests(user_id):
+    join_requests = get_join_requests(user_id)
+
+    if join_requests == []:
+        return {"message": "No join requests"}, 200
+    
+    return {"join_requests": join_requests}, 200
