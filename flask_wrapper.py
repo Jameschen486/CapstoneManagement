f8c1fa87b81dc58f3d06d20cea3659963a63198e
AndyWang9797 <andywang971@gmail.com>
2024-06-25 18:18:32 +1000

    Added view group details view join_request and handle join request functions

--- flask_wrapper.py
+++ flask_wrapper.py
@@ -40,5 +40,27 @@ def join_group_route():
     response, status_code = groups.join_group(group_id, student_id, MAX_STUDENT_PER_GROUP)
     return jsonify(response), status_code
 
+@app.route('/group/request/handle', methods=['POST'])
+def handle_join_request_route():
+    data = request.form
+    creator_id = data.get('creator_id')
+    applicant_id = data.get('applicant_id')
+    group_id = data.get('group_id')
+    accept = data.get('accept', 'false').lower() == 'true'
+    response, status_code = groups.handle_join_request(creator_id, applicant_id, group_id, accept, MAX_STUDENT_PER_GROUP)
+    return jsonify(response), status_code
+
+@app.route('/group/', methods=['GET'])
+def view_group_details_route():
+    group_id = request.args.get('group_id')
+    response, status_code = groups.view_group_details(group_id)
+    return jsonify(response), status_code
+
+@app.route('/user/join_requests', methods=['GET'])
+def view_join_requests_route():
+    user_id = request.args.get('user_id')
+    response, status_code = groups.view_join_requests(user_id)
+    return jsonify(response), status_code
+
 if __name__ == "__main__":
     app.run(debug=True)
