c77f7932253739835902fe3f53d47ae34311f5e1
Jennifer Sung <z5259396@student.unsw.edu.au>
2024-06-25 21:36:54 +1000

    frontend docker setup

--- backend/server.py
+++ backend/server.py
@@ -33,4 +33,4 @@ def auth_register():
     return jsonify(register(email, password, firstName, lastName))
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
