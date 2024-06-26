a3e84139a47c9d405daedec9abbb9690d9ff9be0
AndyWang9797 <andywang971@gmail.com>
2024-06-26 21:55:10 +1000

    fixed parameter names

--- backend/server.py
+++ backend/server.py
@@ -65,11 +65,9 @@ def get_user():
 @app.route('/group/create', methods=['POST'])
 def create_group_endpoint():
     data = request.form
-    group_name = data.get('name')
-    group_description = data.get('description')
-    creator_id = data.get('creator_id')
-
-    response, status_code = groups.create_group(group_name, group_description, creator_id)
+    group_name = data.get('groupname')
+    creator_id = data.get('ownerid')
+    response, status_code = groups.create_group(group_name, creator_id)
     return jsonify(response), status_code
 
 @app.route('/groups/view', methods=['GET'])
@@ -79,37 +77,37 @@ def view_groups_route():
 @app.route('/group/join', methods=['POST'])
 def join_group_route():
     data = request.form
-    group_id = data.get('group_id')
-    student_id = data.get('student_id')
+    group_id = data.get('groupid')
+    student_id = data.get('userid')
     response, status_code = groups.join_group(group_id, student_id, MAX_STUDENT_PER_GROUP)
     return jsonify(response), status_code
 
 @app.route('/group/request/handle', methods=['POST'])
 def handle_join_request_route():
     data = request.form
-    creator_id = data.get('creator_id')
-    applicant_id = data.get('applicant_id')
-    group_id = data.get('group_id')
-    accept = data.get('accept', 'false').lower() == 'true'
-    response, status_code = groups.handle_join_request(creator_id, applicant_id, group_id, accept, MAX_STUDENT_PER_GROUP)
+    user_id = data.get('userid')
+    applicant_id = data.get('applicantid')
+    group_id = data.get('groupid')
+    accept = data.get('accept')
+    response, status_code = groups.handle_join_request(user_id, applicant_id, group_id, accept, MAX_STUDENT_PER_GROUP)
     return jsonify(response), status_code
 
 @app.route('/group/', methods=['GET'])
 def view_group_details_route():
-    group_id = request.args.get('group_id')
+    group_id = request.args.get('groupid')
     response, status_code = groups.view_group_details(group_id)
     return jsonify(response), status_code
 
 @app.route('/user/join_requests', methods=['GET'])
 def view_join_requests_route():
-    user_id = request.args.get('user_id')
+    user_id = request.args.get('userid')
     response, status_code = groups.view_join_requests(user_id)
     return jsonify(response), status_code
 
 @app.route('/group/leave', methods=['POST'])
 def leave_group_route():
     data = request.form
-    user_id = data.get('user_id')
+    user_id = data.get('userid')
     response, status_code = groups.leave_group(user_id)
     return jsonify(response), status_code
 
