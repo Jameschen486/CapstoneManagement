a811213161197904af554b7b1deeecb9a9062ab1
xiongliang <z5364790@ad.unsw.edu.au>
2024-07-13 11:48:14 +1000

    add auth to project related routes, change tests accordingly

--- backend/projects.py
+++ backend/projects.py
@@ -9,15 +9,15 @@ class Project:
 
 class Project:
     def __init__(self, 
-                 project_id:int, 
-                 owner_id:int,
+                 projectid:int, 
+                 ownerid:int,
                  title:str, 
                  clients:str = None,    
                  specializations:str = None,  
-                 group_count:int = 0,
+                 groupcount:int = 0,
                  background:str = None, 
                  requirements:str = None, 
-                 req_knowledge:str = None, 
+                 reqknowledge:str = None, 
                  outcomes:str = None, 
                  supervision:str = None,
                  additional:str = None,
@@ -28,15 +28,15 @@ class Project:
         This constructor is treated as private
         '''
         
-        self.project_id = project_id
-        self.owner_id = owner_id
+        self.projectid = projectid
+        self.ownerid = ownerid
         self.title = title
         self.clients = clients
         self.specializations = specializations
-        self.group_count = group_count
+        self.groupcount = groupcount
         self.background = background
         self.requirements = requirements
-        self.req_knowledge = req_knowledge
+        self.reqknowledge = reqknowledge
         self.outcomes = outcomes
         self.supervision = supervision
         self.additional = additional
@@ -45,7 +45,7 @@ class Project:
 
 
     def title_is_valid(title:str) -> bool:
-        return title != None
+        return title is not None
 
 
     def title_exist(title:str) -> bool:
@@ -54,7 +54,7 @@ class Project:
         return title in titles_list
         
 
-    def create(title:str, owner_id:int):
+    def create(title:str, ownerid:int):
         '''
         create a project in database
         '''
@@ -63,36 +63,36 @@ class Project:
         if Project.title_exist(title):
             raise InputError(description="Project with the same title already exists")
 
-        dummy_project_info = vars(Project(None, owner_id, title))
-        dummy_project_info.pop("project_id")
+        dummy_project_info = vars(Project(None, ownerid, title))
+        dummy_project_info.pop("projectid")
         dummy_project_info.pop("channel")
 
-        project_id = dbAcc.create_project(*dummy_project_info.values())
+        projectid = dbAcc.create_project(*dummy_project_info.values())
 
-        return {"message": "Project created.", "project_id": project_id}, 201
+        return {"message": "Project created.", "projectid": projectid}, 201
        
 
-    def get_details(project_id: int, user_id: int):
-        project = Project.load(project_id)
-        if (project == None):
-            raise InputError(description=f"Project with id {project_id} does not exist")
-        if user_id != project.owner_id:
+    def get_details(projectid: int, userid: int):
+        project = Project.load(projectid)
+        if (project is None):
+            raise InputError(description=f"Project with id {projectid} does not exist")
+        if userid != project.ownerid:
             # Tutor might be allowed to get details of other projects
-            raise AccessError(description=f"Project with id {project_id} is not your project.")
+            raise AccessError(description=f"Project with id {projectid} is not your project.")
 
         response = vars(project)
         return response, 200
 
 
-    def load(project_id:int) -> Project:
+    def load(projectid:int) -> Project:
         '''
         Load a project from database
         '''
-        if (project_id == None):
+        if (projectid is None):
             return None
         
-        project_info = dbAcc.get_project_by_id(project_id)
-        project = Project(*project_info) if (project_info != None) else None
+        project_info = dbAcc.get_project_by_id(projectid)
+        project = Project(*project_info) if (project_info is not None) else None
         return project
     
 
@@ -104,7 +104,7 @@ class Project:
         projects = dict()
         for project_info in projects_info:
             project = Project(*project_info)
-            projects[project.project_id] = project
+            projects[project.projectid] = project
 
         return projects
 
@@ -115,18 +115,18 @@ class Project:
         This is done by creating a new project with the same id.
         '''
        
-        user_id = int(data['user_id'])
-        project_id = data.get('project_id', default=None, type=int)
+        userid = int(data['userid'])
+        projectid = data.get('projectid', default=None, type=int)
         title = data.get('title', default=None)
-        old_project = Project.load(project_id)
+        old_project = Project.load(projectid)
 
         # Check 1: existence of the project
-        if old_project == None:
-            raise InputError(description=f"Project with id {project_id} does not exist")
+        if old_project is None:
+            raise InputError(description=f"Project with id {projectid} does not exist")
         # Check 2: premission of updating the project
-        if user_id != old_project.owner_id:
+        if userid != old_project.ownerid:
             # Tutor might be allowed to update other projects
-            raise AccessError(description=f"Project with id {project_id} is not your project.")
+            raise AccessError(description=f"Project with id {projectid} is not your project.")
         # Check 3: no duplicate title
         if Project.title_exist(title):
             # Also, new title can't be identical to the original title
@@ -136,15 +136,15 @@ class Project:
             title = old_project.title
 
         new_project = Project(
-            project_id,
-            user_id,
+            projectid,
+            userid,
             title,
             clients=data.get('clients', default=old_project.clients),
             specializations=data.get('specializations', default=old_project.specializations),
-            group_count=data.get('group_count', default=old_project.group_count, type=int),
+            groupcount=data.get('groupcount', default=old_project.groupcount, type=int),
             background=data.get('background', default=old_project.background),
             requirements=data.get('requirements', default=old_project.requirements),
-            req_knowledge=data.get('req_knowledge', default=old_project.req_knowledge),
+            reqknowledge=data.get('reqknowledge', default=old_project.reqknowledge),
             outcomes=data.get('outcomes', default=old_project.outcomes),
             supervision=data.get('supervision', default=old_project.supervision),
             additional=data.get('additional', default=old_project.additional),
@@ -156,22 +156,20 @@ class Project:
         new_project_info.pop('channel')
         dbAcc.update_project(*new_project_info.values())
 
-        return {"message": "Project updated.", "project_id": project_id}, 200
+        return {"message": "Project updated.", "projectid": projectid}, 200
 
 
-    def delete(user_id:int, project_id:int):
-        project = Project.load(project_id)
+    def delete(userid:int, projectid:int):
+        project = Project.load(projectid)
         if (project == None):
-            raise InputError(description=f"Project with id {project_id} does not exist")
-        if (project.owner_id != user_id):
+            raise InputError(description=f"Project with id {projectid} does not exist")
+        if (project.ownerid != userid):
             # Tutor might be allowed to update other projects
-            raise AccessError(description=f"Project with id {project_id} is not your project")
+            raise AccessError(description=f"Project with id {projectid} is not your project")
         
-        dbAcc.delete_project_by_id(project_id)
-        #print("calling dbAcc.create_project() with arguements: ", end="")
-        #print(project_id)
+        dbAcc.delete_project_by_id(projectid)
 
-        return {"message": "Project deleted.", "project_id": project_id}, 200
+        return {"message": "Project deleted.", "projectid": projectid}, 200
 
 
 
