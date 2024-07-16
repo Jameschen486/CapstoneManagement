537335437ebb3473082927a90481b9f461e9bdcb
AndyWang9797 <andywang971@gmail.com>
2024-07-16 19:08:05 +1000

    modified group tests

--- backend/tests/test_groups.py
+++ backend/tests/test_groups.py
@@ -1,81 +1,83 @@
-from tests.helper import CLIENT, truncate, get_user
+from tests import helper
+from permission import Role
+
 
 def test_create_group():
-    truncate()
-    user_id = get_user(0)
+    helper.truncate()
+    user_id, token = helper.create_user(role=Role.STUDENT)
     
-    response = CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id})
+    response = helper.CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}, headers = helper.token2headers(token))
     assert response.status_code == 201
     data = response.get_json()
     assert data["message"] == "Group created successfully!"
     assert "group_id" in data
 
 def test_view_groups():
-    truncate()
-    user_id = get_user(0)
-    CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id})
+    helper.truncate()
+    user_id, token = helper.create_user(role=Role.STUDENT)
+    helper.CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}, headers = helper.token2headers(token))
 
-    response = CLIENT.get('/groups/view')
+    response = helper.CLIENT.get('/groups/view', headers = helper.token2headers(token))
     assert response.status_code == 200
     data = response.get_json()
     assert len(data) == 1
     assert data[0]["groupname"] == "Test Group"
 
 def test_join_group():
-    truncate()
-    user_id = get_user(0)
-    group_id = CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}).get_json()["group_id"]
-    student_id = get_user(1)
+    helper.truncate()
+    user_id, token = helper.create_user(role=Role.STUDENT)
+    group_id = helper.CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}, headers = helper.token2headers(token)).get_json()["group_id"]
+    student_id, token2 = helper.create_user(role=Role.STUDENT)
     
-    response = CLIENT.post('/group/join', data={"groupid": group_id, "userid": student_id})
+    response = helper.CLIENT.post('/group/join', data={"groupid": group_id, "userid": student_id}, headers = helper.token2headers(token2))
     assert response.status_code == 201
     data = response.get_json()
     assert data["message"] == "Join request sent successfully!"
 
 def test_handle_join_request():
-    truncate()
-    user_id = get_user(0)
-    group_id = CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}).get_json()["group_id"]
-    student_id = get_user(1)
-    CLIENT.post('/group/join', data={"groupid": group_id, "userid": student_id})
+    helper.truncate()
+    user_id, token = helper.create_user(role=Role.STUDENT)
+    group_id = helper.CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}, headers = helper.token2headers(token)).get_json()["group_id"]
+    student_id, token2 = helper.create_user(role=Role.STUDENT)
+    helper.CLIENT.post('/group/join', data={"groupid": group_id, "userid": student_id}, headers = helper.token2headers(token2))
     
-    response = CLIENT.post('/group/request/handle', data={"userid": user_id, "applicantid": student_id, "groupid": group_id, "accept": True})
+    response = helper.CLIENT.post('/group/request/handle', data={"userid": user_id, "applicantid": student_id, "groupid": group_id, "accept": True}, headers = helper.token2headers(token))
     assert response.status_code == 201
     data = response.get_json()
     assert data["message"] == f"User {student_id} added to your group."
 
 def test_view_group_details():
-    truncate()
-    user_id = get_user(0)
-    group_id = CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}).get_json()["group_id"]
+    helper.truncate()
+    user_id, token = helper.create_user(role=Role.STUDENT)
+    group_id = helper.CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}, headers = helper.token2headers(token)).get_json()["group_id"]
     
-    response = CLIENT.get('/group', query_string={"groupid": group_id})
+    response = helper.CLIENT.get('/group', query_string={"groupid": group_id}, headers = helper.token2headers(token))
     assert response.status_code == 200
     data = response.get_json()
     assert data["groupid"] == group_id
     assert data["groupname"] == "Test Group"
 
 def test_view_join_requests():
-    truncate()
-    user_id = get_user(0)
-    group_id = CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}).get_json()["group_id"]
-    student_id = get_user(1)
-    CLIENT.post('/group/join', data={"groupid": group_id, "userid": student_id})
+    helper.truncate()
+    user_id, token = helper.create_user(role=Role.STUDENT)
+    group_id = helper.CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}, headers = helper.token2headers(token)).get_json()["group_id"]
+    student_id, token2 = helper.create_user(role=Role.STUDENT)
+    helper.CLIENT.post('/group/join', data={"groupid": group_id, "userid": student_id}, headers = helper.token2headers(token2))
     
-    response = CLIENT.get('/user/join_requests', query_string={"userid": user_id})
+    response = helper.CLIENT.get('/user/join_requests', query_string={"userid": user_id}, headers = helper.token2headers(token))
     assert response.status_code == 200
     data = response.get_json()
     assert "join_requests" in data
 
 def test_leave_group():
-    truncate()
-    user_id = get_user(0)
-    group_id = CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}).get_json()["group_id"]
-    student_id = get_user(1)
-    CLIENT.post('/group/join', data={"groupid": group_id, "userid": student_id})
-    CLIENT.post('/group/request/handle', data={"userid": user_id, "applicantid": student_id, "groupid": group_id, "accept": True})
+    helper.truncate()
+    user_id, token = helper.create_user(role=Role.STUDENT)
+    group_id = helper.CLIENT.post('/group/create', data={"groupname": "Test Group", "ownerid": user_id}, headers = helper.token2headers(token)).get_json()["group_id"]
+    student_id, token2 = helper.create_user(role=Role.STUDENT)
+    helper.CLIENT.post('/group/join', data={"groupid": group_id, "userid": student_id}, headers = helper.token2headers(token2))
+    helper.CLIENT.post('/group/request/handle', data={"userid": user_id, "applicantid": student_id, "groupid": group_id, "accept": True}, headers = helper.token2headers(token))
     
-    response = CLIENT.post('/group/leave', data={"userid": student_id})
+    response = helper.CLIENT.post('/group/leave', data={"userid": student_id})
     assert response.status_code == 200
     data = response.get_json()
     assert data["message"] == "User has left the group"
@@ -83,7 +85,6 @@ def test_leave_group():
 
 print("1")
 test_create_group()
-print("2")
 test_view_groups()
 test_join_group()
 test_handle_join_request()
