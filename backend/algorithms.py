ca0c2c757cc8558a0c905cf668893c8055d8c0b0
chichun2002 <chichun2002@gmail.com>
2024-07-17 18:22:18 +1000

    algorithm v1

---
+++ backend/algorithms.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.optimize import linear_sum_assignment
+
+groups = [{"id" : 1, "skills" : [{'python': 2}, {'frontend': 1}]},
+          {"id" : 2, "skills" : [{'python' : 3}, {'backend': 1}]},
+          {"id" : 3, "skills" : [{'c': 1}, {'java': 1}, {'python': 1}, {'frontend': 1}]},
+          {"id" : 4, "skills" : [{'frontend': 2}, {'backend': 1}, {'python': 1}]}]
+
+projects = [{"id" : 1, "skills" : ['python', 'frontend']},
+            {"id" : 2, "skills" : ['c', 'frontend']},
+            {"id" : 3, "skills" : ['python', 'backend']},
+            {"id" : 4, "skills" : ['java', 'frontend', 'backend']},
+            {"id" : 5, "skills" : ['python']}]
+
+# matrix = np.zeros((max(len(groups),len(projects)),max(len(groups),len(projects))))
+matrix = np.zeros((len(groups),len(projects)))
+
+for i, group in enumerate(groups):
+    for j, project in enumerate(projects):
+        score = 0
+        for skill in group['skills']:
+            if skill.keys() in project['skills']:
+                score+=skill.values()
+        matrix[i][j] = score
+        # print(group['id'], project['id'], score)
+
+print(matrix)
+row_ind, col_ind = linear_sum_assignment(matrix, maximize = True)
+# print(col_ind)
+# print(row_ind)
+
+print(matrix[row_ind, col_ind].sum())
+
+for i, entry in enumerate(row_ind):
+    print(groups[entry]['id'], projects[col_ind[i]]['id'])
