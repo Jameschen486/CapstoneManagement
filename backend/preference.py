e5f346dd446446613f5147b2c2dcb9fbf076ae84
AndyWang9797 <andywang971@gmail.com>
2024-07-17 19:01:56 +1000

    Group members(if in a group) can now view preference, added a max value for rank

--- backend/preference.py
+++ backend/preference.py
@@ -2,7 +2,16 @@ import dbAcc
 from error import InputError, AccessError
 from permission import Role
 
+MAX_NO_OF_PREFERNCE = 7
+
 def add_preference(student_id, project_ids, ranks):
+    
+    # Check if there are duplicated ranks
+    if len(ranks) != len(set(ranks)):
+        return {"message": "Duplicated ranks"}, 400
+    
+    if any(rank > MAX_NO_OF_PREFERNCE for rank in ranks):
+        return {"message": "Rank exceeded maximum"}, 400
 
     existing_preferences = dbAcc.get_user_preferences(student_id)
     existing_project_ids = [pref.projectid for pref in existing_preferences]
@@ -21,13 +30,26 @@ def add_preference(student_id, project_ids, ranks):
 # Not too sure about our design rn, this function currently replaces ALL the preference list with the new passed in preferences
 def edit_preference(student_id, project_ids, ranks):
 
+    # Check if there are duplicated ranks
+    if len(ranks) != len(set(ranks)):
+        raise InputError(description="Duplicated ranks")
+
     dbAcc.delete_preferences(student_id)
     dbAcc.create_preferences(student_id, project_ids, ranks)
     return {"message": "Preferences updated successfully!"}, 200
 
 def view_preference(user_id, student_id, role):
-    # Allow the student to view their own preferences or roles with higher access right now clients, coordinator and admin
-    if user_id == student_id or role in [Role.CLIENT, Role.COORDINATOR, Role.ADMIN]:
+
+    # Allow the student and the group members to view their own preferences or roles with higher access right now clients, coordinator and 
+    viewable_students = []
+    group_id = dbAcc.get_user_by_id(user_id)[6]
+    if group_id is None:
+        viewable_students.append(student_id)
+    else:
+        for member in dbAcc.get_group_members(dbAcc.get_user_by_id(user_id)[6]):
+            viewable_students.append(member[0])
+    
+    if user_id in viewable_students or role in [Role.CLIENT, Role.COORDINATOR, Role.ADMIN]:
         preferences = dbAcc.get_user_preferences(student_id)
         return [{"project_id": pref.projectid, "rank": pref.rank} for pref in preferences], 200
     else:
