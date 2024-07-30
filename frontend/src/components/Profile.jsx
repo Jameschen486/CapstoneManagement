35c91e405386e754e8de9184645c50b25ec81a79
Jennifer Sung <z5259396@student.unsw.edu.au>
2024-07-31 02:03:40 +1000

    student skills list on profile complete

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -8,12 +8,13 @@ const Profile = () => {
   const [role, setRole] = useState('');
   const [hasData, setHasData] = useState(false);
   const [user, setUser] = useState({});
-  const [showNotificationModal, setShowNotificationModal] = useState(false);
-  const [showMessageModal, setShowMessageModal] = useState(false);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [isUserDetailSectionCollapsed, setIsUserDetailSectionCollapsed] = useState(true);
+  const [skills, setSkills] = useState([]);
+  const [studentSkills, setStudentSkills] = useState([]);
+  const [isSkillsSectionCollapsed, setIsSkillsSectionCollapsed] = useState(true);
 
   useEffect(() => {
     // User role from localStorage
@@ -25,6 +26,8 @@ const Profile = () => {
       navigate('/');
     }
     fetchUserData();
+    fetchStudentSkills();
+    fetchSkills();
   }, [navigate]);
 
   const fetchUserData = async () => {
@@ -90,8 +93,138 @@ const Profile = () => {
   const toggleUserDetailSection = () => {
     setIsUserDetailSectionCollapsed(!isUserDetailSectionCollapsed);
   };
+
+  const toggleSkillsSection = () => {
+    setIsSkillsSectionCollapsed(!isSkillsSectionCollapsed);
+  };
+
   const handleBack = () => {
     navigate('/dashboard');
+  };
+
+  const fetchSkills = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const userId = localStorage.getItem('userId');
+
+      const response = await fetch(`http://localhost:5001/skills/view?userid=${userId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        // Convert the object of skills to an array
+        const skillsArray = Object.values(data);
+        setSkills(skillsArray);
+      } else {
+        throw new Error('Failed to load skills');
+      }
+    } catch (error) {
+      console.error('Error fetching skills:', error);
+      alert('Error fetching skills');
+    }
+  };
+
+  useEffect(() => {
+    fetchSkills();
+  }, []);
+
+  const fetchStudentSkills = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const userId = localStorage.getItem('userId');
+      const studentId = parseInt(localStorage.getItem('role'), 10);
+  
+      if (isNaN(studentId) || studentId > 0) {
+        throw new Error("Invalid student ID");
+      }
+  
+      const response = await fetch(`http://localhost:5001/skills/view/student?userid=${userId}&studentid=${userId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      if (!response.ok) {
+        const text = await response.text();
+        console.log('Server response:', text);
+        throw new Error(text);
+      }
+  
+      const data = await response.json();
+      console.log(data);
+      const studentSkillsArray = Object.entries(data).map(([key, value]) => ({ skillid: parseInt(key), skillname: value }));
+      setStudentSkills(studentSkillsArray);
+    } catch (error) {
+      console.error('Error fetching student skills:', error);
+      alert(`Error fetching student skills: ${error.message}`);
+    }
+  };
+  
+  const handleAddSkill = async (skillId) => {
+    try {
+      const token = localStorage.getItem('token');
+      const userId = localStorage.getItem('userId');
+
+      const formData = new FormData();
+      formData.append('userid', userId);
+      formData.append('studentid', userId);
+      formData.append('skillid', skillId);
+
+      const response = await fetch('http://localhost:5001/skill/add/student', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+
+      const text = await response.text();
+      if (!response.ok) {
+        console.log('Server response:', text);
+        throw new Error(text);
+      }
+
+      alert('Skill added successfully!');
+      fetchStudentSkills(); // Refresh the list of student skills
+    } catch (error) {
+      console.error('Error adding skill:', error);
+      alert(`Error adding skill: ${error.message}`);
+    }
+  };
+
+  const handleRemoveSkill = async (skillId) => {
+    try {
+      const token = localStorage.getItem('token');
+      const userId = localStorage.getItem('userId');
+
+      const formData = new FormData();
+      formData.append('userid', userId);
+      formData.append('studentid', userId);
+      formData.append('skillid', skillId);
+
+      const response = await fetch('http://localhost:5001/skill/remove/student', {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+
+      const text = await response.text();
+      if (!response.ok) {
+        throw new Error(text);
+      }
+
+      alert('Skill removed successfully!');
+      fetchStudentSkills();
+    } catch (error) {
+      console.error('Error removing skill:', error);
+      alert(`Error removing skill: ${error.message}`);
+    }
   };
 
   return (
@@ -103,20 +236,11 @@ const Profile = () => {
       <div className="dashboard-profile-content">
         <div className="dashboard-profile-section">
           <h2>User Detail</h2>
-            <div className='dashboard-profile-row'>
-                {console.log(user.first)}
-                <p><strong>Firstname:</strong>
-                  {firstName}
-                </p>
-                
-                <p><strong>LastName:</strong>
-                  {lastName}
-                </p>
-                
-                <p><strong>Email:</strong>
-                <p>{email}</p>
-                </p>       
-            </div>
+          <div className='dashboard-profile-row'>
+            <p><strong>Firstname:</strong> {firstName}</p>
+            <p><strong>LastName:</strong> {lastName}</p>
+            <p><strong>Email:</strong> <p>{email}</p></p>
+          </div>
           <button onClick={toggleUserDetailSection} className="toggle-button">
             {isUserDetailSectionCollapsed ? 'Update User Detail' : 'Close'}
           </button>
@@ -132,17 +256,43 @@ const Profile = () => {
               <button type="submit">Save Changes</button>
             </form>
           )}
+          <h2 className='skills-header'>User Skills</h2>
           <div className='user-skills-section'>
-            <h2>User Skills</h2>
+            <h3 className='skills-list'>Skills List</h3>
+            <ul>
+              <p>Click on the skill to delete it from your list</p>
+              {studentSkills.length > 0 ? (
+                studentSkills.map((skill) => (
+                  <li key={skill.skillid} onClick={() => handleRemoveSkill(skill.skillid)}>
+                    {skill.skillname}
+                  </li>
+                ))
+              ) : (
+                <li>No skills found</li>
+              )}
+            </ul>
+            <button onClick={toggleSkillsSection} className="toggle-button">
+              {isSkillsSectionCollapsed ? 'Add Skill' : 'Close'}
+            </button>
+            {!isSkillsSectionCollapsed && (
+              <div className='skills-content'>
+                <p>Click on a skill to add it to your list:</p>
+                <ul>
+                  {skills.length > 0 ? (
+                    skills.map((skill) => (
+                      <li key={skill.skillid} onClick={() => handleAddSkill(skill.skillid)}>
+                        {skill.skillname}
+                      </li>
+                    ))
+                  ) : (
+                    <li>No skills found</li>
+                  )}
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
-      <Modal show={showNotificationModal} handleClose={() => setShowNotificationModal(false)} title="Notifications">
-        <p>No notifications yet.</p>
-      </Modal>
-      <Modal show={showMessageModal} handleClose={() => setShowMessageModal(false)} title="Messages">
-        <p>No messages yet.</p>
-      </Modal>
     </div>
   );
 };
